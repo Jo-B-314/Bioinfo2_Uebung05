e6ee8a6899925a06d062203fcb618f9f5f3b0c51
Lisa-Marie Rolli <s8liroll@stud.uni-saarland.de>
2021-05-20 16:52:02 +0200

    bla

--- aufgabe2.R
+++ aufgabe2.R
@@ -10,14 +10,17 @@ data <- read.table("Methotrexate_drug_response_binarized_and_gene_expression.txt
 set.seed(42)
 #a) we split our data so we have 80% in our training set and 20% in the test set
 sample = sample.split(data, SplitRatio = 0.8)
-train = subset(data, sample == TRUE)
+training = subset(data, sample == TRUE)
 test  = subset(data, sample == FALSE)
 #b) 5 fold cross validation
 train_control <- trainControl(method = "cv", number = 5)
+model <- train(Methotrexate ~ .,  data = training, trControl = train_control,
+               method = "rf", metric = "roc")
+print(model)
 # Train the model
-#for (mtry in 1:70) {
-#  rf <- randomForest(formula = Sensitivity ~., data = train, mtry = mtry,
-#               trControl = train_control, metric = "roc")
-#}
+for (mtry in 1:70) {
+  rf <- randomForest(formula = Methotrexate ~., data = training, mtry = mtry,
+               trControl = train_control, metric = "roc")
+}
 # Summarize the results
-#print(rf)
+print(rf)
